a17ef79b1392375414d1e29090b6dfbb8b5b3a93
xkr <194636027@qq.com>
2021-06-18 20:07:06 +0800

    Update exam.py
    试卷model

--- exam/models/exam.py
+++ exam/models/exam.py
@@ -1,20 +1,22 @@
+from datetime import datetime
+
 from sqlalchemy import ForeignKey
 from exam import db
 
 
 class Paper(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String)
-    strt_t = db.Column(db.DateTime)
-    end_t = db.Column(db.DateTime)
-    duration = db.Column(db.DateTime)
-    published = db.Column(db.Boolean)
-    end = db.Column(db.Boolean)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    subject = db.Column(db.String, nullable=False)
+    strt_t = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    end_t = db.Column(db.DateTime, nullable=False)
+    duration = db.Column(db.DateTime, nullable=False)
+    published = db.Column(db.Boolean, nullable=False)
+    end = db.Column(db.Boolean, nullable=False)
 
 
 class exam_has_problem(db.Model):
-    exam_id = db.Column(db.Integer, ForeignKey("paper.id"))
-    pro_id = db.Column(db.Integer, ForeignKey("problem.problem_id"))
+    exam_id = db.Column(db.Integer, ForeignKey("paper.id"), nullable=False)
+    pro_id = db.Column(db.Integer, ForeignKey("problem.problem_id"), nullable=False)
 
 
 
