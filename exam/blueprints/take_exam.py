d9a0909f505008e5fe9e7ee25a1356d5a512b360
Smh <76023060+Kona-Sumon@users.noreply.github.com>
2021-06-19 21:28:09 +0800

    增加了考试信息界面，以及对考试是否可以开始的判断

--- exam/blueprints/take_exam.py
+++ exam/blueprints/take_exam.py
@@ -7,3 +7,8 @@ take_exam_bp = Blueprint('take_exam', __name__)
 @take_exam_bp.route('/')
 def home():
     return '这是学生参加考试的主页面。'
+
+
+@take_exam_bp.route('/take_exam')
+def take_exam():
+    return '这是开始考试的界面'
--- exam/blueprints/view_exam.py
+++ exam/blueprints/view_exam.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 from exam import db
 from exam.models.exam import Paper, exam_has_problem
 from flask import render_template, request, flash, redirect, url_for
+from datetime import datetime
+import time
 
 view_exam_bp = Blueprint('view_exam', __name__)
 
@@ -10,3 +12,18 @@ view_exam_bp = Blueprint('view_exam', __name__)
 def home():
     exams = Paper.query.all()
     return render_template('exam/view_exam.html', exams=exams)
+
+
+@view_exam_bp.route('/information/<int:paper_id>')
+def show_information(paper_id):
+    exam = Paper.query.filter_by(paper_id=paper_id).first()
+    dt1 = time.time()
+    n_time = datetime.strftime(exam.strt_t, "%Y-%m-%d")
+    dt2 = time.mktime(time.strptime(n_time, "%Y-%m-%d"))
+    diff = dt1-dt2
+
+    if diff < 0:
+        label = False
+    else:
+        label = True
+    return render_template('exam/exam_info.html', exam=exam, label=label)
