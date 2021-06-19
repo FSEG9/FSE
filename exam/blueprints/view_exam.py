69a432d10cb9cb58384e9454c41bdc1553e7f475
Smh <76023060+Kona-Sumon@users.noreply.github.com>
2021-06-19 16:06:11 +0800

    e
    ???

--- exam/blueprints/view_exam.py
+++ exam/blueprints/view_exam.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
-
+from exam import db
+from exam.models.exam import Paper, exam_has_problem
+from flask import render_template, request, flash, redirect, url_for
 
 view_exam_bp = Blueprint('view_exam', __name__)
 
 
 @view_exam_bp.route('/')
 def home():
-    return '这是学生考试信息查看的主页面。'
+    exams = Paper.query.all()
+    return render_template('exam/view_exam.html', exams=exams)
