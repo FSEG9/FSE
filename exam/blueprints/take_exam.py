cc0651636c4d9aca8706beb23db39c6dab175fee
Smh <76023060+Kona-Sumon@users.noreply.github.com>
2021-06-19 21:48:28 +0800

    添加显示答卷界面，添加结束时间判断

--- exam/blueprints/take_exam.py
+++ exam/blueprints/take_exam.py
@@ -12,3 +12,8 @@ def home():
 @take_exam_bp.route('/take_exam')
 def take_exam():
     return '这是开始考试的界面'
+
+
+@take_exam_bp.route('/show_exam')
+def show_exam():
+    return '这是显示答卷的界面'
--- exam/blueprints/view_exam.py
+++ exam/blueprints/view_exam.py
@@ -19,11 +19,14 @@ def show_information(paper_id):
     exam = Paper.query.filter_by(paper_id=paper_id).first()
     dt1 = time.time()
     n_time = datetime.strftime(exam.strt_t, "%Y-%m-%d")
-    dt2 = time.mktime(time.strptime(n_time, "%Y-%m-%d"))
-    diff = dt1-dt2
+    dt2 = time.mktime(time.strptime(n_time, "%Y-%m-%d"))  # 开始时间
+    n_time = datetime.strftime(exam.end_t, "%Y-%m-%d")
+    dt3 = time.mktime(time.strptime(n_time, "%Y-%m-%d"))  # 结束时间
 
-    if diff < 0:
-        label = False
+    if dt1<dt2:
+        label = 0
+    elif dt1>dt3:
+        label = 1
     else:
-        label = True
+        label = 2
     return render_template('exam/exam_info.html', exam=exam, label=label)
