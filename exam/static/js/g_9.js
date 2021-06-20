8fc5518ed36a3eda47a0d2a63f5f8beb1598b89c
xkr <194636027@qq.com>
2021-06-20 18:45:25 +0800

    手动生成试卷

---
+++ exam/static/js/g_9.js
@@ -0,0 +1,28 @@
+$(function() {
+    "use strict";
+    $(document).on('click', '.done-btn', function () {
+        var $item = $(this).parent().parent().parent();
+        var $this = $(this);
+
+        if ($item.data('chosen')) {
+            $.ajax({
+                type: 'PATCH',
+                url: $this.data('href'),
+                success: function (data) {
+                    $item.data('chosen', false);
+                    M.toast({html: data.message});
+                }
+            })
+        } else {
+            $.ajax({
+                type: 'PATCH',
+                url: $this.data('href'),
+                success: function (data) {
+                    $item.data('chosen', true);
+                    M.toast({html: data.message});
+                }
+            })
+        }
+    });
+
+})
