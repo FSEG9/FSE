7ebc64f05fb78d7cfb6161d472b3209438e11e10
xkr <194636027@qq.com>
2021-06-26 23:31:51 +0800

    xxx

--- exam/models/answer.py
+++ exam/models/answer.py
@@ -10,22 +10,22 @@ exam_has_anspaper = db.Table(
 
 class Anspaper(db.Model):
     anspaper_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    paper_id = db.Column(db.Integer, db.ForeignKey('paper.paper_id'))
+    paper_id = db.Column(db.Integer, db.ForeignKey('paper.paper_id',ondelete='CASCADE'))
     student_id = db.Column(db.Integer)
     score_all = db.Column(db.Integer)
     Answers = db.relationship("Anspa_prob_answer", lazy='dynamic')
 
 
 class Anspa_prob_answer(db.Model):
-    anspaper_id = db.Column(db.Integer, db.ForeignKey('anspaper.anspaper_id'), nullable=False, primary_key=True)
-    problem_id = db.Column(db.Integer, db.ForeignKey('problem.problem_id'), nullable=False, primary_key=True)
+    anspaper_id = db.Column(db.Integer, db.ForeignKey('anspaper.anspaper_id',ondelete='CASCADE'), nullable=False, primary_key=True)
+    problem_id = db.Column(db.Integer, db.ForeignKey('problem.problem_id',), nullable=False, primary_key=True)
     answer = db.Column(db.String(5))
     score = db.Column(db.Integer)
 
 # class exam_has_probanls(db.Model):
 
 class ProbAnalysis(db.Model):
-    exam_id = db.Column(db.Integer, db.ForeignKey('paper.paper_id'), nullable=False, primary_key=True)
+    exam_id = db.Column(db.Integer, db.ForeignKey('paper.paper_id',ondelete='CASCADE'), nullable=False, primary_key=True)
     problem_id = db.Column(db.Integer, db.ForeignKey('problem.problem_id'), nullable=False, primary_key=True)
     A_count = db.Column(db.Integer, default=0)
     B_count = db.Column(db.Integer, default=0)
--- exam/models/exam.py
+++ exam/models/exam.py
@@ -6,22 +6,24 @@ from exam.models.answer import exam_has_anspaper
 exam_has_problem = db.Table(
     'exam_has_problem',
     db.Column('problem_id', db.Integer, db.ForeignKey('problem.problem_id')),
-    db.Column('paper_id', db.Integer, db.ForeignKey('paper.paper_id'))
+    db.Column('paper_id', db.Integer, db.ForeignKey('paper.paper_id', ondelete='CASCADE'))
 )
 
 
 
-class Paper(db.Model):
-    name = db.Column(db.String(50), nullable=False)
-    paper_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    subject = db.Column(db.String(40), nullable=False)
-    strt_t = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    end_t = db.Column(db.DateTime, nullable=False)
-    end = db.Column(db.Boolean,  default=False)  # 是否已经结束
-    score = db.Column(db.Float, default=-1)  # 平均分
-    anlsflag = db.Column(db.Boolean, default=False)  # 是否已经分析
-    problems = db.relationship('Problem',
-                               secondary=exam_has_problem,
-                               back_populates='papers')
-    anspapers = db.relationship("Anspaper", lazy='dynamic')
-    probanls = db.relationship('ProbAnalysis')
+# class Paper(db.Model):
+#     name = db.Column(db.String(50), nullable=False)
+#     paper_id = db.Column(db.Integer, primary_key=True, nullable=False)
+#     subject = db.Column(db.String(40), nullable=False)
+#     strt_t = db.Column(db.DateTime, default=datetime.now, nullable=False)
+#     end_t = db.Column(db.DateTime, nullable=False)
+#     end = db.Column(db.Boolean,  default=False)  # 是否已经结束
+#     score = db.Column(db.Float, default=-1)  # 平均分
+#     anlsflag = db.Column(db.Boolean, default=False)  # 是否已经分析
+#     problems = db.relationship('Problem',
+#                                secondary=exam_has_problem,
+#                                back_populates='papers')
+#     anspapers = db.relationship("Anspaper", lazy='dynamic')
+#     probanls = db.relationship('ProbAnalysis')
+#     to_class = db.Column(db.Integer, db.ForeignKey("student_to_course.course_id", ondelete='CASCADE'))
+
